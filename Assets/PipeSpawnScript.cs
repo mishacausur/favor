ace3d4e83f0bec31e37b5055db1d61db3c8ba6a4
Misha Causur <mishacausur@mail.ru>
2023-01-06 21:15:10 +0500

    trees moveing added

--- Assets/PipeSpawnScript.cs
+++ Assets/PipeSpawnScript.cs
@@ -7,6 +7,7 @@ public class PipeSpawnScript : MonoBehaviour
     public GameObject pipe;
     public float spawnRate = 2;
     private float timer = 0;
+    public float heightOffset = 0.1f;
 
     // Start is called before the first frame update
     void Start()
@@ -17,20 +18,22 @@ public class PipeSpawnScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         if (timer < spawnRate)
         {
             timer += Time.deltaTime;
         }
-        else 
+        else
         {
             spawnPipe();
-             timer = 0;
+            timer = 0;
         }
-       
     }
 
     void spawnPipe()
     {
-        Instantiate(pipe, transform.position, transform.rotation);
+        float lowerPoint = transform.position.y - heightOffset;
+        float highestPoint = transform.position.y + heightOffset;
+        Instantiate(pipe, new Vector3(transform.position.x, Random.Range(lowerPoint, highestPoint), 0), transform.rotation);
     }
 }
